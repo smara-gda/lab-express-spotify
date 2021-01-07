22bc5ca632dbb6a39048010912e4d6a242ce38b9
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-01-07 14:14:49 +0100

    iteration 4

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
@@ -11,6 +11,7 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // setting the spotify-api goes here:
 const spotifyAPI = new SpotifyWebApi({
@@ -36,7 +37,7 @@ app.get('/artist-search', (req, res) => {
     .searchArtists(myQuery)
     .then((results) => {
       const artists = results.body.artists.items;
-      //   console.log('got an answer:', results.body.artists.items[0]);
+      // console.log('got an answer:', results.body.artists.items[0]);
       res.render('artist-search-results', {
         myQuery,
         artists
@@ -45,6 +46,16 @@ app.get('/artist-search', (req, res) => {
     .catch((error) => {
       console.log('I was unable to fetch the requested information', error);
     });
+});
+
+app.get('/albums/:id', (req, res) => {
+  const id = req.params.id;
+  // console.log('Got album id', id);
+  spotifyAPI.getArtistAlbums(id).then((album) => {
+    const albums = album.body.items;
+    console.log(albums);
+    res.render('albums', { albums });
+  });
 });
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
